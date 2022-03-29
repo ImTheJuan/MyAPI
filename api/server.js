af94152a69599321e1d13e1512875069314d00f9
ImTheJuan <90651382+ImTheJuan@users.noreply.github.com>
2022-03-29 14:13:14 +0200

    2.0

--- api/server.js
+++ api/server.js
@@ -5,8 +5,9 @@ const app = require('./app');
 
 // Setup server port
 const port = process.env.PORT || 3000;
+const db = process.env.MONGODB || 'mongodb://127.0.0.1:27017/API';
 
-mongoose.connect('mongodb://127.0.0.1:27017/API', (err) => {
+mongoose.connect(db, (err) => {
   if (err) console.log('Error connecting MongoDB');
   else {
     app.listen(port, () => {
